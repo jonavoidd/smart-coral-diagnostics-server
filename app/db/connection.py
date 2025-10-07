bb154c7ac107836be5802433f0989a5147109011
Kurt <kurtjonathangozano@gmail.com>
2025-10-07 21:24:51 +0800

    updated database connection string to use the supabase conn str

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,11 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+from app.core.config import settings
 from app.models import *
 
-DATABASE_URL = (
-    "postgresql://postgres:postgres@localhost:5432/smart_coral?sslmode=disable"
-)
+
+DATABASE_URL = settings.TRANSACTION_POOLER
+
+if not DATABASE_URL:
+    raise ValueError("DatabaseUrl is not set on .env")
+
+# DATABASE_URL = (
+#     "postgresql://postgres:postgres@localhost:5432/smart_coral?sslmode=disable"
+# )
 
 engine: Engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
