b08dc4634ee6303332981496e4dc7fa1ed8f8146
Kurt <kurtjonathangozano@gmail.com>
2025-10-07 16:28:57 +0800

    added website url on origins

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:8000"],
+    allow_origins=["http://localhost:3000", "http://localhost:8000", "https://smart-coral-diagnostics.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
